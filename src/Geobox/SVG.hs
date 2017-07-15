4c086a7c5f11da241d74359bde0da6e006c1127b
sjc <s.cole.j@gmail.com>
2017-07-15 15:40:38 +1000

    Triangle WIP.

--- src/Geobox/SVG.hs
+++ src/Geobox/SVG.hs
@@ -2,9 +2,10 @@ module Geobox.SVG
   ( renderSVGElements
   ) where
 
-import Text.XML.Light.Types
-import Geobox.Paper.Mark
 import Geobox.EastNorth
+import Geobox.Paper.Colour
+import Geobox.Paper.Mark
+import Text.XML.Light.Types
 
 qname :: String -> QName
 qname s = QName s Nothing Nothing
@@ -16,3 +17,36 @@ class SVGRenderable a where
 instance SVGRenderable Mark where
   renderSVGElements (Mark (EastNorth east north) kind colour) =
     [Element (qname "polygon") [] [] Nothing]
+
+data V2 =
+  V2 Double
+     Double
+
+type Transform2D = V2 -> V2
+
+triangle :: Transform2D -> [Element]
+triangle t =
+  [ Element
+      (qname "polygon")
+      [ Attr (qname "points") (renderV2 (map t equilateralTrianglePoints))
+      , Attr (qname "style") "fill:none,stroke:black"
+      ]
+      []
+      Nothing
+  ]
+
+halfRoot3 :: Double
+halfRoot3 = sqrt 3 / 2
+
+equilateralTrianglePoints :: [V2]
+equilateralTrianglePoints = [V2 0 halfRoot3, V2 1 (-halfRoot3), V2 (-1) (-halfRoot3)]
+
+wrapSvg :: [Element] -> Element
+wrapSvg es = Element (qname "svg") [] (map Elem es) Nothing
+
+renderV2 :: [V2] -> String
+renderV2 vs = unwords (map f vs)
+  where f (V2 x y) = show x ++ "," ++ show y
+
+scale :: Double -> Transform2D
+scale s (V2 x y) = V2 (x * s) (y * s)
